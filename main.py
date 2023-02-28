bb95a6302481f2e71a8a553f8e6cbf6a3e9cfcfb
danillyamkin <danilwebdev@gmail.com>
2023-02-28 23:15:34 +0300

    Add acf graph of the best individual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-from constants import POPULATION_SIZE
+from constants import POPULATION_SIZE, CODE_SEQUENCE_LENGTH
+from individual import Individual
 from population import Population
 
 
@@ -10,9 +12,11 @@ def main():
     max_fitness_values = []
     mean_fitness_values = []
 
+    best_fitness = 0
+    best_individual = None
+
     generation_counter = 0
 
-    # while max(fitness_values) <= 8.6 and generation_counter < 100:
     while generation_counter < 100:
         generation_counter += 1
 
@@ -32,15 +36,39 @@ def main():
         mean_fitness_values.append(mean_fitness)
         best_index = fitness_values.index(max(fitness_values))
 
+        if max_fitness > best_fitness:
+            best_fitness = max_fitness
+            best_individual = Individual.clone(population[best_index])
+
         print(f"The population {generation_counter}: Max fitness = {max_fitness}, Mean fitness = {mean_fitness}")
         print("The best individual = ", *population[best_index], "\n")
 
     # charts
-    plt.plot(max_fitness_values, color='red')
-    plt.plot(mean_fitness_values, color='green')
+    show_max_mean_fitness_graph(max_fitness_values, mean_fitness_values)
+    show_acf_graph(best_individual)
+
+
+def show_max_mean_fitness_graph(max_fitness, mean_fitness):
+    plt.plot(max_fitness, color='red')
+    plt.plot(mean_fitness, color='green')
     plt.xlabel('Generation')
     plt.ylabel('Max / Mean fitness')
     plt.title('Dependence of max and mean fitness on generation')
+    plt.show()
+
+
+def show_acf_graph(individual):
+    right_acf = individual.calc_acf()
+    left_acf = reversed(right_acf)
+    acf = [*left_acf, CODE_SEQUENCE_LENGTH, *right_acf]
+
+    length_of_side = len(right_acf)
+
+    y = np.arange(-length_of_side, length_of_side + 1, 1)
+
+    plt.title('ACF of the best individual')
+    plt.grid(True)
+    plt.plot(y, acf)
     plt.show()
 
 
